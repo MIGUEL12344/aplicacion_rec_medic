f212246624de98a53d820d1bcce6e2e1025cf98e
MIGUEL12344 <ronalmiguel2005@gmail.com>
2024-12-05 09:58:19 -0500

    corregido

--- app_rec_medic/app_rec_medic.py
+++ app_rec_medic/app_rec_medic.py
@@ -1,6 +1,10 @@
 import reflex as rx
 from .screen.splash_screen import splashScreen
 from .screen.home_screen import homeScreen
+from .screen.search_screen import searchScreen
+from .screen.about_user_screen import aboutUserScreen
+from .screen.about_2 import aboutUser2Screen
+from .screen.notify_screen import notifyScreen
 from rxconfig import config
 
 
